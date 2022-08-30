5792ef076f3de454d9dbd422b6c117c52550bfc9
JeanMaxTB <jmaxtb@gmail.com>
2022-08-30 10:16:59 -0500

    second commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let decimal = 0;
+  for (let i = 0; i < num.toString().length; i++) {
+    decimal = decimal + parseInt(num.toString()[num.toString().length - (i + 1)]) * (2**i);
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let bin = [];
+  while (num >= 1) {
+    bin.unshift(num%2);
+    num = Math.floor(num / 2);
+  }
+  return bin.join("");
 }
 
 
